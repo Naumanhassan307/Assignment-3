e8e2a2a8753cc413e76f896387c91e9a65db711e
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-05-05 14:19:19 -0700

    start work on Assignment 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 
 function App() {
   return <div className="App">
-    
-    
+      <Dropdown>
+    <Dropdown.Toggle variant="success" id="dropdown-basic">
+      Dropdown Button
+    </Dropdown.Toggle>
+
+    <Dropdown.Menu>
+      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
+
   </div>;
 }
 
